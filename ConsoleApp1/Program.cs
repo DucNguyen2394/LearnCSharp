807908edba36740dcfa2be7b2dc4d46a1ce2d9d6
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-08-07 15:08:33 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("my heart");
+			Console.Title = "My first app C#";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*****************************");
+            Console.WriteLine("***     0 0 0   0 0 0     ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("***     0 0 0 0 0 0 0     ***");
+            Console.WriteLine("***       0 0 0 0 0       ***");
+            Console.WriteLine("***         0 0 0         ***");
+            Console.WriteLine("***           0           ***");
+            Console.WriteLine("*****************************");
+        }
+
+	
+	}
+}
