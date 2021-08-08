b68142f20fa6d28e833bd71240cde6dc1e508d41
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-08-08 18:25:25 +0700

    update

--- CaroUniversity/Program.cs
+++ CaroUniversity/Program.cs
@@ -10,7 +10,12 @@ namespace CaroUniversity
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Caro university of Venice has the following change: ");
+            Console.WriteLine("1.Increased the administrative costs by about 40 percent ");
+            Console.WriteLine("2.Replace of administration facilities for the growing faculty and the students ");           
+            Console.WriteLine("3.Build a system of courses available for students to search ");
+            Console.WriteLine("4.Students can see tuition fees ");
+            Console.WriteLine("5.Students registration ");
         }
     }
 }
---
+++ Session02/TronExpress/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TronExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int totalSalary = 0;
+
+                Console.WriteLine("Nhap so goi hang ban duoc: ");
+                int n = int.Parse(Console.ReadLine());
+                totalSalary = 50 * n + 75;
+
+                Console.WriteLine("Co lam ca dem hay khong: Y/N");
+                String choose = Console.ReadLine();
+
+                if (choose.ToLower().Contains("y"))
+                {
+                    totalSalary = (110 * totalSalary) / 100; 
+                }
+
+                Console.WriteLine("tong luong = " + totalSalary);
+
+                Console.WriteLine("Ban co muon thoat hay khong: Y/N ");
+                String c = Console.ReadLine();
+                if (c.ToLower().Contains("y"))
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
