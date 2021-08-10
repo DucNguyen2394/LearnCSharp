7194545d83f37551db06b229610539fdc289223b
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-08-10 09:14:39 +0700

    update

---
+++ Lab_01/AnonTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_01
+{
+    class AnonTypes
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new { Name = "A", Price = 3 };
+            Console.WriteLine("Name = {0}\nPrice = {1}",
+                              p1.Name.ToLower(), p1.Price);
+            Console.ReadLine();
+
+        }
+
+    }
+}
