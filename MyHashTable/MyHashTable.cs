aefd48cce56115fb431fa52cddcbdbb3a1408e15
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-08-19 08:25:19 +0700

    update

---
+++ MyHashTable/MyHashTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyHashTable
+{
+    class MyHashTable
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,3,2};
+
+            Hashtable map = new Hashtable();
+
+            foreach(int a in arr)
+            {
+                if(map.ContainsKey(a) == false)
+                {
+                    map.Add(a, 1);
+                }
+                else
+                {
+                    int count = map.Count;
+                    count++;
+                    map.Add(a,count);
+                }
+            }
+        }
+    }
+}
--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -4,65 +4,57 @@ namespace QuickSort
 {
     class Program
     {
-        static void Sort(int[] a, int l, int r)
+        static void Sort(int[] arr, int l, int r)
         {
             if (l >= r) return;
+            int key = arr[(l + r) / 2];
 
-            int key = a[(l + r) / 2];
+            int mid = Partition(arr, l, r, key);
 
-            int mid = Partition(a, l, r, key);
+            Sort(arr, l, mid - 1);
 
-            Sort(a, l , mid - 1);
-
-            Sort(a, mid, r);
+            Sort(arr, mid, r);
         }
 
-        static int Partition(int[] a, int l, int r, int key)
+        static int Partition(int[] arr, int l, int r, int key)
         {
-            int middle = (l + r) / 2;
             while (l <= r)
             {
-                while (a[l] < key ) l++;
+                while (arr[l] < key) l++;
 
-                while (a[r] > key) r--;
+                while (arr[r] > key) r--;
 
-                if (l <= r)
+                if(l <= r)
                 {
-                    int tmp = a[l];
-
-                    a[l] = a[r];
-
-                    a[r] = tmp;
-
-                    l++; r--;
-
+                    int tmp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = tmp;
+                    l++;r--;
                 }
-                
+
             }
 
             return l;
         }
-        
+
         static void Main(String[] args)
         {
-            //int[] arr = { 1,3,4,2,6,8,1};
-            int[] arr = { 29, 40, 63, 54, 17,12,3,56,67,23,12,3,46,32,9 };
+            int[] arr = {54,2,2,6,6,3,123,6,78,54,12,5,78,34,67,98,4,123,45,78,23,987,45,12,34,677,6,23,76,87,99,23,12};
+            Console.WriteLine("Mang chua sap xep: ");
 
-            Console.WriteLine("mang chua sap xep");
+            foreach(var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+
+            Console.WriteLine("Mang da sap xep: ");
+
+            Sort(arr, 0, arr.Length - 1);
 
             foreach (var a in arr)
             {
                 Console.Write(a + " ");
             }
-            Console.WriteLine(" ");
-            Console.WriteLine("mang da chua sap xep");
-            Sort(arr, 0, arr.Length-1);
-
-            foreach (var a in arr)
-            {
-                Console.Write(a + " ");
-            }
-
         }
-    } 
+    }
 }
